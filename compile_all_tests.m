9d1d27c960882caab3f0ba3ae1045aace7e5d13b
Adriano Ruseler <ruseler@utfpr.edu.br>
2025-12-01 11:23:22 -0300

    Add more tests

--- compile_all_tests.m
+++ compile_all_tests.m
@@ -47,3 +47,53 @@ for i = 1:length(toolNames)
         end
     end  
 end
+
+%% Create info quiz
+clc
+clear all
+
+infos.xmlpath=[pwd '\xmlfiles']; % xmlfiles
+infos.fname='Kroki-tests';  
+
+[status, cmdout] = system('curl -s http://localhost:8000/health');
+% Assuming cmdout contains the JSON string
+jsonStruct = jsondecode(cmdout);
+
+% 1. Extract the structure containing all the versions
+versionStruct = jsonStruct.version;
+
+% 2. Get a list of all field names (the tool names)
+toolNames = fieldnames(versionStruct);
+
+% List all files inside each targetDir and display their names
+q=1;
+for i = 1:length(toolNames)
+    targetDir = fullfile('tests/', toolNames{i});
+    % List all files inside each targetDir and display their names
+    files = dir(targetDir);
+    % Exclude the current directory and parent directory from the list
+    files = files(~ismember({files.name}, {'.', '..'}));
+    files = files(~endsWith({files.name}, {'.md','.svg'})); % Exclude .md and .svg files from the list
+
+    % Display the total number of files found in the directory
+    fprintf('Total files in %s: %d\n', targetDir, length(files));
+
+    % Create curl command for each file
+    for f=1:length(files)
+        % Execute the curl command for the current file
+        curl_command = sprintf('curl -s -X POST -H "Content-Type: text/plain" --data-binary "@%s" http://localhost:8000/%s/svg', fullfile(targetDir, files(f).name), toolNames{i});
+        [status, cmdout] = system(curl_command);
+        if status == 0
+            encoded_string = matlab.net.base64encode(cmdout);
+            imgstr=['<img src="data:image/svg+xml;base64,' encoded_string '">'];
+            infos.q(q).text=['<code>' curl_command '</code><p>' imgstr '</p>'];
+            infos.q(q).name=[toolNames{i} ': ' files(f).name];
+            q=q+1;
+        else
+            disp(['Error executing curl command for ', toolNames{i}, '. Status: ', num2str(status)]);
+        end
+    end  
+end
+
+
+info2ioxml(infos)
--- tests/wavedrom/README.md
+++ tests/wavedrom/README.md
@@ -3,3 +3,6 @@
 Version: 3.5.0
 
 - https://github.com/wavedrom/wavedrom
+- https://wavedrom.com/tutorial.html
+- https://wavedrom.com/tutorial2.html
+- https://wavedrom.com/editor.html
