f0eea158f5546aba741b0b421ba8ca0c0697146b
Adriano Ruseler <ruseler@utfpr.edu.br>
2025-12-01 15:16:24 -0300

    Test kroki-diagramsnet

--- compile_all_tests.m
+++ compile_all_tests.m
@@ -57,7 +57,7 @@ jsonStruct = jsondecode(cmdout);
 versionStruct = jsonStruct.version;
 toolNames = fieldnames(versionStruct);
 
-buildfor='dbml'; % Change this
+buildfor='diagramsnet'; % Change this
 targetDir = fullfile('tests/', buildfor);
 % List all files inside each targetDir and display their names
 files = dir(targetDir);
--- tests/diagramsnet/README.md
+++ tests/diagramsnet/README.md
@@ -1,3 +1,5 @@
 # diagramsnet
 
 Version: 16.2.4
+
+- https://www.drawio.com/example-diagrams
