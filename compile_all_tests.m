b8da09045f7a3d2c80201647a678fcf540e35b51
Adriano Ruseler <ruseler@utfpr.edu.br>
2025-12-01 16:24:37 -0300

    Add ERD examples

--- compile_all_tests.m
+++ compile_all_tests.m
@@ -57,7 +57,7 @@ jsonStruct = jsondecode(cmdout);
 versionStruct = jsonStruct.version;
 toolNames = fieldnames(versionStruct);
 
-buildfor='ditaa'; % Change this
+buildfor='erd'; % Change this
 targetDir = fullfile('tests/', buildfor);
 % List all files inside each targetDir and display their names
 files = dir(targetDir);
--- tests/erd/README.md
+++ tests/erd/README.md
@@ -1,3 +1,5 @@
 # erd
 
 Version: 0.2.3
+
+- https://github.com/BurntSushi/erd
