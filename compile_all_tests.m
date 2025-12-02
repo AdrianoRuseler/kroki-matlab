d264616b5685ce76248ec2b5a65358db6f213d99
Adriano Ruseler <ruseler@utfpr.edu.br>
2025-12-02 13:06:27 -0300

    Add structurizr examples

--- compile_all_tests.m
+++ compile_all_tests.m
@@ -46,7 +46,7 @@ clear all
 
 % toolNames = getkrokitools();
 
-buildfor='svgbob'; % Change this
+buildfor='structurizr'; % Change this
 targetDir = fullfile('tests/', buildfor);
 % List all files inside each targetDir and display their names
 files = dir(targetDir);
