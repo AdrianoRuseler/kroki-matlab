97a199f4d1724b5fe7d98deeb02be79878ac939b
Adriano Ruseler <ruseler@utfpr.edu.br>
2025-12-02 12:51:35 -0300

    Add packetdiag example

--- compile_all_tests.m
+++ compile_all_tests.m
@@ -46,7 +46,7 @@ clear all
 
 % toolNames = getkrokitools();
 
-buildfor='plantuml'; % Change this
+buildfor='packetdiag'; % Change this
 targetDir = fullfile('tests/', buildfor);
 % List all files inside each targetDir and display their names
 files = dir(targetDir);
--- tests/packetdiag/README.md
+++ tests/packetdiag/README.md
@@ -1,3 +1,5 @@
 # packetdiag
 
 Version: 3.1.0
+
+- http://blockdiag.com/en/nwdiag/packetdiag-examples.html
