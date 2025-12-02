62ebde48f41ed74a62707599aafcd45c545e731e
Adriano Ruseler <ruseler@utfpr.edu.br>
2025-12-02 13:39:09 -0300

    Add UmLet examples

--- compile_all_tests.m
+++ compile_all_tests.m
@@ -46,7 +46,7 @@ clear all
 
 % toolNames = getkrokitools();
 
-buildfor='symbolator'; % Change this
+buildfor='umlet'; % Change this
 targetDir = fullfile('tests/', buildfor);
 % List all files inside each targetDir and display their names
 files = dir(targetDir);
--- tests/umlet/README.md
+++ tests/umlet/README.md
@@ -1,3 +1,6 @@
 # umlet
 
 Version: 15.1
+
+- https://github.com/umlet/umlet
+- https://www.umlet.com/
