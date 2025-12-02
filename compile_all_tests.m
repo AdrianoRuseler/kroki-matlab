3d3c64ecd1e38dc2a23a64869bd049ca1d3879a3
Adriano Ruseler <ruseler@utfpr.edu.br>
2025-12-02 12:58:42 -0300

    Add svgbob examples

--- compile_all_tests.m
+++ compile_all_tests.m
@@ -46,7 +46,7 @@ clear all
 
 % toolNames = getkrokitools();
 
-buildfor='packetdiag'; % Change this
+buildfor='svgbob'; % Change this
 targetDir = fullfile('tests/', buildfor);
 % List all files inside each targetDir and display their names
 files = dir(targetDir);
--- tests/svgbob/README.md
+++ tests/svgbob/README.md
@@ -1,3 +1,10 @@
 # svgbob
 
+Svgbob Editor
+
+Convert your ascii diagram scribbles into happy little SVG
+
 Version: 0.7.6
+
+- https://github.com/ivanceras/svgbob
+- https://ivanceras.github.io/svgbob-editor/
